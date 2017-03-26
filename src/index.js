d66fd44f42dd9c9150ab197b2e6e7255d2a16b1e
John Chang <johnchang9094@gmail.com>
2017-03-26 21:33:19 +0100

    Added class base components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/searchbar';
+
+const API_KEY = 'AIzaSyCks9RoaWqvMLm8umozkwFBT51T50q4k6g';
+
 // Create a new component. This component should produce some HTML.
 
-const App = function() {
-  return <div>Hi!</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+    );
 }
 
 // Take his component's generated HTML and put it on the page. (in the DOM)
 
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
