2eb79958d6a21054f005f2e3025e3b9e0f7a1a51
John Chang <johnchang9094@gmail.com>
2017-03-27 00:12:12 +0100

    Adding props

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCks9RoaWqvMLm8umozkwFBT51T50q4k6g';
 
-YTSearch({key: API_KEY, term: 'surfBoards'}, function(data) {
-  console.log(data);
-});
-
 // Create a new component. This component should produce some HTML.
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfBoards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
     );
+  };
 }
 
 // Take his component's generated HTML and put it on the page. (in the DOM)
