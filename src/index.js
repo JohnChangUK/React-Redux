f23fba3d1007c2c9700aac9eaa0de539d8951d6b
John Chang <johnchang9094@gmail.com>
2017-03-27 23:47:00 +0100

    Implemented const url with backticks es6 syntax

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/searchbar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCks9RoaWqvMLm8umozkwFBT51T50q4k6g';
 
@@ -12,10 +13,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+       };
 
     YTSearch({key: API_KEY, term: 'surfBoards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -23,10 +30,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList videos={this.state.videos} />
       </div>
     );
-  };
+  }
 }
 
 // Take his component's generated HTML and put it on the page. (in the DOM)
